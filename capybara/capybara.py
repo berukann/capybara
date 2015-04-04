401abe66ed31f00776d4d4bbbb83b25d2681b72d
AkihikoITOH <itoh.akihiko.osaka@gmail.com>
2015-04-04 23:30:13 +0900

    fixed accidental delete of capybara.py

--- capybara/capybara.py
+++ capybara/capybara.py
@@ -2,34 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
-import argparse
-
 from amazon_wrapper import AmazonWrapper
 from rakuten_wrapper import RakutenWrapper
 
-aw = AmazonWrapper()
-rw = RakutenWrapper()
+class Capybara:
+    def __init__(self, config_dir=None, tokens_dir=None):
+        self.wrappers = {}
+        self.wrappers['amazon'] = AmazonWrapper()
+        self.wrappers['rakuten'] = RakutenWrapper()
 
-def get(service, item=None):
-    if service == 'rakuten':
-        return rw.access_wrapper({'item_id': '%s' % item})
-    elif service == 'amazon':
-        return aw.access_wrapper({'asin': '%s' % item})
-    else:
-        return None
+        for service, wrapper in self.wrappers.items():
+            config_filename = './%s_config.json' % service
+            tokens_filename = './%s_tokens.tsv' % service
+            config_path = os.path.normpath(os.path.join(os.getcwd(), config_dir, config_filename))
+            tokens_path = os.path.normpath(os.path.join(os.getcwd(), tokens_dir, tokens_filename))
+            wrapper.setup(config_path, tokens_path)
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='Lemur: Wrapper for multiple tokens of multiple APIs')
-    parser.add_argument('service', choices=['amazon', 'rakuten'], help="Service name")
-    parser.add_argument('item', help="Item id")
-
-    args = parser.parse_args()
-
-    service = args.service
-    item = args.item
-
-    print get(service, item)
-
-
+    def get(self, service=None, item=None):
+        return self.wrappers[service].access_wrapper({'item': item})
 
